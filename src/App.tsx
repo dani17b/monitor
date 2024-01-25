7bdee0ef5fc69bb5c7870eebe8dac79e2fa97cdc
Daniel Turiño Anido <danieltani@servicioexterno.inditex.com>
2024-01-25 09:09:26 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './app.css';
+//import './app.css';
 import { Home } from './modules/home';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Home />
     </div>
   );
